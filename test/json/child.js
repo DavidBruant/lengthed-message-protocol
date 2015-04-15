4ea43e28012a304d6037391c9e21c6b2eca01874
David Bruant <bruant.d@gmail.com>
2015-04-16 01:26:22 +0200

    JSON seem to be passed along fine

---
+++ test/json/child.js
@@ -0,0 +1,23 @@
+"use strict";
+
+var fs = require('fs');
+var Channel = require('../../Channel.js');
+
+function makeStreamsToParent(fd){
+    return {
+        read: fs.createReadStream(undefined, {fd: fd}),
+        write: fs.createWriteStream(undefined, {fd: fd})
+    }
+}
+
+var parentStreams = makeStreamsToParent(3);
+
+var c = new Channel(parentStreams.read, parentStreams.write);
+
+c.on('message', function(msg){
+    var m = JSON.parse(msg);
+    m.a++;
+    m.b++;
+    
+    c.send(JSON.stringify(m));
+});
---
+++ test/json/parent.js
@@ -0,0 +1,24 @@
+"use strict";
+
+var spawn = require('child_process').spawn;
+var Channel = require('../../Channel.js');
+
+var child = spawn('node', [__dirname + '/child.js'], {
+    // 4th pipe is for communication
+    stdio: [0, 1, 2, 'pipe']
+});
+
+var c = new Channel(child.stdio[3], child.stdio[3]);
+
+var o = {
+    a: 1, 
+    b: 2
+}
+
+c.send(JSON.stringify(o));
+
+c.on('message', function(msg){    
+    var m = JSON.parse(msg);
+    
+    console.log(m.a === o.a+1 && m.b === o.b+1);
+});
